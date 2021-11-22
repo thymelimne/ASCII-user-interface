03342b0ea1af80a6fd371010d916544ca4330eea
ThymeLimne Tech <91765107+thymelimne@users.noreply.github.com>
2021-11-21 22:09:59 -0500

    Update tui.hpp

--- tui.hpp
+++ tui.hpp
@@ -9,16 +9,16 @@ struct COORD {int x;int y;};
 TUI: Text User Interface. Like a GUI, but it's text that gets printed to the console, then eventually printed over when the program updates it.
 This TUI takes an object-oriented approach, with a tree structure of 'Grid' objects.
 _________________
-                | <-- Here is an example of a picture you might draw.
+                |		<--- Here is an example of a picture you might draw.
        ##       |	
-     ######     |	But, what if you wanted a dashboard about this plant, at the corner of the screen?
-   ##########   |		...Maybe, to display about how hydrated it is?
+	 ######     |			But, what if you wanted a dashboard about this plant, at a corner of the screen?
+   ##########   |												...Maybe, to display about how hydrated it is?
      ######     |				
-       ##   #   |		That is why this program takes an object-oriented approach.
-       ## ##    |
-       ####     |			Each object holds something you want to draw, and where you want to draw it,
-       ##       |			so you could easily define another 'Grid' object, that you'd name 'dashboard',
-%%%%%%%%%%%%%%%%%			and then set its location to wherever you like on the canvas. (...& then reposition it.)
+	   ##   #   |				That is why this program takes an object-oriented approach.
+	   ## ##    |
+	   ####     |					Each object holds something you want to draw, and where you want to draw it,
+	   ##       |					so you could easily define another 'Grid' object, that you'd name 'dashboard',
+%%%%%%%%%%%%%%%%%					and then set its location to wherever you like on the canvas. (...& then reposition it.)
 
 
 
@@ -28,7 +28,7 @@ _________________
 
 
 To use:
- 1. First, note that this relies on the SetConsoleCursorPosition() method given by <Windows.h>.
+ 1. First, note that the cruicial code for this relies on the SetConsoleCursorPosition() method given by <Windows.h>.
 	So, it'll probably only work for your coding project if you're using Windows... Apologies, otherwise.
  2. Include this file as a header.
  3. In your main code, initialize the class somewhere, in the following way:
@@ -41,11 +41,11 @@ To use:
 
  _._     _,-'""`-._
 (,-.`._,'(       |\`-/|
-    `-.-' \ )-`( , o o)
-          `-    \`_`"'-
+	`-.-' \ )-`( , o o)
+		  `-    \`_`"'-
 
-	>>  std::string drawing = " _._     _,-'""`-._\n(,-.`._,'(       |\`-/|\n    `-.-' \ )-`( , o o)\n          `-    \`_`"'-";
-	>>  Grid foxPicture{drawing};
+	>>  std::string drawing = " _._     _,-'""`-._\n(,-.`._,'(       |\`-/|\n    `-.-' \ )-`( , o o)\n          `-    \`_`"'-"
+	>>	Grid foxPicture{drawing};
 	>>  canvas.append(foxPicture, {0,25}) //'location'={0,25}, to place the drawing leftmost, and toward the bottom of the whole tapestry.
 
 			NOTES ABOUT LOCATION:
@@ -69,7 +69,22 @@ To use:
    |
    V
 
-   
+To display your drawings, call the function:
+
+	>>  canvas.display(); //Display all the drawings on the output console.
+
+There is another class function, currently empty, called prepare(). This is left open for a programmer.
+To use this function, create a class that inherits from Grid, and define prepare() in that.
+
+	>>  class tetrisGame : class Grid { public:
+	>>		void prepare(T data)
+	>>			{****Write your implementation of this method here...****}
+	>>  };
+
+What 'prepare()' is supposed to do, is take data that suggests how it should change its drawing, 
+ and change either its own drawing, or a drawing of one of its subGrids, in a desired way.
+
+
 */
 template <typename T>
 class Grid
